cd0f2adc73876b1b3c4738e1429474179bc2a398
BrianMBowden <brianmbowden@live.ca>
2021-05-25 17:45:39 -0600

    setting up boilerplate scene and playing around with three js library

--- portfoliopage/main.js
+++ portfoliopage/main.js
@@ -1,6 +1,27 @@
-import './style.css'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 500);
+
+/* const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+}); */
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
+document.body.appendChild(renderer.domElement);
+
+
+function animate() {
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+}
+
+animate();
